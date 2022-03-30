0c207344fbd23252e31fee83109ce22809c25f7b
Sean-OConnor-VA <sean.oconnor@va.gov>
2022-03-30 19:22:33 -0400

    day 1 done

---
+++ day-1/bandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is the name of your favorite Pet? ")
+
+bandName = city + pet
+print("your band name is: " + bandName + "!")
