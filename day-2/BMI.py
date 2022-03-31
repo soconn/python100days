99f91322137caedd1deb8a0e5531c1e1ae73f27e
Sean-OConnor-VA <sean.oconnor@va.gov>
2022-03-30 20:35:49 -0400

    code challenges complete for day 2

---
+++ day-2/BMI.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+h=float(height)
+w=float(weight)
+nh = h**2
+
+# print(h)
+# print(w)
+# print(nw)
+bmi = int(w/nh)
+
+
+
+print(str(bmi))
--- day-2/dataTypes.py
+++ day-2/dataTypes.py
@@ -1,4 +1,10 @@
-#Data Types
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
 
-#Strings
-    #Strings need Double Qotes
+####################################
+#Write your code below this line 👇
+a = str(two_digit_number)
+
+num = int(a[0]) + int(a[1])
+print(num)
