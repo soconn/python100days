bf3230dcc88dd6e837f279e4f06c42dedb998ef1
Sean-OConnor-VA <sean.oconnor@va.gov>
2022-06-07 22:39:52 -0400

    day 3 done

--- day-3/LoveCalculator.py
+++ day-3/LoveCalculator.py
@@ -9,4 +9,30 @@ name2 = input("What is their name? \n").lower()
 print(name1)
 print(name2)
 
+combined_string = name1 + name2
 
+t=combined_string.count("t")
+r=combined_string.count("r")
+u=combined_string.count("u")
+e=combined_string.count("e")
+
+true = t+r+u+e
+
+l=combined_string.count("l")
+o=combined_string.count("o")
+v=combined_string.count("v")
+e=combined_string.count("e")
+
+love=l+o+v+e
+
+score=int(str(true) + str(love))
+# print(f"Your score is " + {score})
+
+score = int(score)
+
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos!")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you'll be alright together")
+else:
+    print(f"Your Score is {score}! You are doomed")
