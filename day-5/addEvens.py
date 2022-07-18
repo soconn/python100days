7f64cd5beee3cb2a1b67fc66d6f0bc246ebce9cf
Sean-OConnor-VA <sean.oconnor@va.gov>
2022-07-17 22:35:40 -0400

    day 5 complete

---
+++ day-5/addEvens.py
@@ -0,0 +1,8 @@
+num = 0
+for i in range(1, 101):
+    if (i % 2) == 0:
+        num += i
+
+
+print(num)
+
---
+++ day-5/forLoopandRange.py
@@ -0,0 +1,4 @@
+new=0
+for number in range(1, 101):
+    new=new + number
+    print(str(new))
