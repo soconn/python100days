7eab7e975c5f081e1a261eb59156b48f11e5cbcc
Sean-OConnor-VA <sean.oconnor@va.gov>
2022-09-19 21:26:26 -0400

    day8 complete

---
+++ day-8/function7.py
@@ -0,0 +1,58 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+# def encrypt (text, shift):
+#     cipher_text = ""
+#     for each_letter in text:
+#         x = alphabet.index(each_letter)
+#         x += shift
+#         newx = alphabet[x]
+#         cipher_text += str(newx)
+#     print(f"The encoded text is: {cipher_text}")
+
+# def decrypt (text, shift):
+#     cipher_text = ""
+#     for each_letter in text:
+#         x = alphabet.index(each_letter)
+#         x -= shift
+#         newx = alphabet[x]
+#         cipher_text += str(newx)
+#     print(f"The decoded text is: {cipher_text}")
+
+def caesar (text, shift, direction):
+    cipher_text = ""
+    for each_letter in text:
+        x = alphabet.index(each_letter)
+        if direction == "encode":
+            x += shift
+        elif direction == "decode":
+            x -= shift
+        else:
+            print(f"{direction} is not a valid input!")
+            exit
+        newx = alphabet[x]
+        cipher_text += str(newx)
+    print(f"The decoded text is: {cipher_text}")
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# if direction == "encode":
+#     encrypt(text, shift)
+# if direction == "decode":
+#     decrypt(text,shift)
+caesar(text, shift, direction)
