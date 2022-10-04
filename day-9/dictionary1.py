9f44daf130d85e20b1c0aacf0c55514d24d07822
Sean O'Connor <61710416+Sean-OConnor-VA@users.noreply.github.com>
2022-10-04 08:07:28 -0400

    9.1

--- day-9/dictionary1.py
+++ day-9/dictionary1.py
@@ -1,1 +1,24 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
+    }
+
+programming_dictionary["Bug"]
+
+# Add new items to dictionary
+
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+
+print(programming_dictionary["Loop"])
+
+empty_dictionary = {}
+
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+programming_dictionary["Bug"] = "This is my newest entry text"
+print(programming_dictionary["Bug"])
+
+#loop through a dictionary with for statement
+for thing in programming_dictionary:
+    print(programming_dictionary[thing])
