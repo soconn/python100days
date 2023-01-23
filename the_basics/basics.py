f38b833a359e48abff1b03ebbd35a21572e96eaa
soconnor@j2interactive.com <sean.oconnor@va.gov>
2023-01-23 17:54:44 -0500

    added thebasics

---
+++ the_basics/basics.py
@@ -0,0 +1,16 @@
+monday_temperatures =[9.1, 8.8, 7.5]
+student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
+
+mysum = sum(student_grades.values)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
+
+
+
+
+
+
+# username = "Python3"
+# lowername = str.lower(username)
+# print(lowername)
---
+++ the_basics/basics2.py
@@ -0,0 +1,7 @@
+monday_temperatures =(1, 4, 5)
+print(monday_temperatures)
+
+
+
+day_temperatures = {"morning": (1.0, 2.0, 3.0), "noon": (4.0, 5.0, 6.0), "evening": (7.0, 8.0, 9.0)}
+#dictionary with tuple values assigned to keys
