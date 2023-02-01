b050efbd923b4d46c75fe6de703aba82ac2d5e5e
Sean O'Connor <61710416+Sean-OConnor-VA@users.noreply.github.com>
2023-02-01 15:46:54 -0500

    stuff

--- pythonMasterClass/Section4/basics.py
+++ pythonMasterClass/Section4/basics.py
@@ -1,3 +1,4 @@
-monday_temperatures = [9.1, 8.8, 7.5]
+monday_temperatures = ['hello', 9.1, 8.8, 7.5, 6.6, 9.9]
 monday_temperatures.append(8.1)
 print(monday_temperatures)
+student_grades = {"Marry": 9.1, "Sim": 8.8, "john": 7.5}
